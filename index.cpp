be1288d728596c870dbbdc9b382e2ce3353cb605
TanninOne <sherb@gmx.net>
2018-07-10 14:12:02 +0200

    avoid keeping the esp open after parsing
    previously the File class was exported directly. However, the C++ class keeps the file open until the object is destroyed. In JS this only happens at an unknown later time when the garbage collector runs, which could lead to the file remaining locked.

--- index.cpp
+++ index.cpp
@@ -1,17 +1,34 @@
 #include "esptk/src/espfile.h"
 #include "nbind/nbind.h"
 #include <vector>
+#include <string>
 
-class ESPFile : public ESP::File {
+class ESPFile {
+private:
+  bool m_IsMaster;
+  bool m_IsDummy;
+  std::string m_Author;
+  std::string m_Description;
+  std::vector<std::string> m_Masters;
+
 public:
-  ESPFile(const std::string &fileName): ESP::File(fileName) {} 
-  std::vector<std::string> masterList() const {
-    std::vector<std::string> result;
-    std::set<std::string> input = masters();
-    std::copy(input.begin(), input.end(), std::back_inserter(result));
+  ESPFile(const std::string &fileName) {
+    ESP::File wrapped(fileName);
+    m_IsMaster = wrapped.isMaster();
+    m_IsDummy = wrapped.isDummy();
+    m_Author = wrapped.author();
+    m_Description = wrapped.description();
 
-    return result;
+    std::set<std::string> input = wrapped.masters();
+    std::copy(input.begin(), input.end(), std::back_inserter(m_Masters));
   }
+
+  bool isMaster() const { return m_IsMaster; }
+  bool isDummy() const { return m_IsDummy; }
+  std::string author() const { return m_Author; }
+  std::string description() const { return m_Description; }
+  std::vector<std::string> masters() const { return m_Masters; }
+  std::vector<std::string> masterList() const { return m_Masters; }
 };
 
 NBIND_CLASS(ESPFile) {
@@ -20,5 +37,6 @@ NBIND_CLASS(ESPFile) {
   getter(isDummy);
   getter(author);
   getter(description);
+  getter(masters);
   getter(masterList);
 }
